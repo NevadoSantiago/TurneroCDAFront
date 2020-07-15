530b1342584f0b3462c1299aad8d7c56d305a84b
Agustin Torchia <agustintorchia@gmail.com>
2020-07-15 10:11:27 -0300

    constantes img y scss added

--- mobile/src/Home/Menu/constantes/urlApi.js
+++ mobile/src/Home/Menu/constantes/urlApi.js
@@ -1,11 +1,11 @@
 
-export const URL_API ="http://192.168.1.105:8081"
+export const URL_API ="http://192.168.0.27:8080"
 
-export const URL_API_USUARIO = "http://192.168.1.105:8080"
+export const URL_API_USUARIO = "http://192.168.0.27:8080"
 
-export const URL_API_ESPECIALIDAD = "http://192.168.1.105:8082"
+export const URL_API_ESPECIALIDAD = "http://192.168.0.27:8080"
 
-export const URL_API_TIENDA = "http://192.168.1.105:8080"
+export const URL_API_TIENDA = "http://192.168.0.27:8080"
 
 
 
