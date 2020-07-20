49af8f5a74a4471dba7c2845c895c73f6f835515
Cristian Leguizamón <cristianleguizamon37@gmail.com>
2020-07-20 14:52:36 -0300

    Revert "Added navbar"
    This reverts commit 79a54ffd8d586f530ab2044f6945ddf8176d9577.

--- web/src/App.js
+++ web/src/App.js
@@ -3,14 +3,28 @@ import logo from './logo.svg';
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import Home from './sites/Home'
 import About from './sites/About'
-import Navbar from './sites/import/Navbar'
 //import './App.css';
 import './style.css'
 
 function App() {
 	return (
 		<Router>
-			<Navbar />
+			<nav className="navbar" role="navigation" aria-label="main navigation">
+				<div class="navbar-brand">
+					<a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+					</a>
+				</div>
+
+				<div id="navbarBasicExample" class="navbar-menu">
+					<div class="navbar-start">
+						<NavLink to="/" className="navbar-item" exact={true} activeClassName='navbar-item active'>Home</NavLink>
+						<NavLink to="/about" className="navbar-item" activeClassName='navbar-item active'>About</NavLink>
+					</div>
+				</div>
+			</nav>
 			<div className="container">
 				<Route path="/" exact component={Home} />
 				<Route path="/about" component={About} />
