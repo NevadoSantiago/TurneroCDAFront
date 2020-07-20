ce5af7a9e06363dbccee10b97cf005118db586e3
Cristian Leguizamón <cristianleguizamon37@gmail.com>
2020-07-20 13:32:18 -0300

    Modified styles, use of sass

--- web/src/App.js
+++ web/src/App.js
@@ -9,14 +9,27 @@ import './style.css'
 function App() {
 	return (
 		<Router>
+			<nav className="navbar" role="navigation" aria-label="main navigation">
+				<div class="navbar-brand">
+					<a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+					</a>
+				</div>
+
+				<div id="navbarBasicExample" class="navbar-menu">
+					<div class="navbar-start">
+						<NavLink to="/" className="navbar-item" exact={true} activeClassName='navbar-item active'>Home</NavLink>
+						<NavLink to="/about" className="navbar-item" activeClassName='navbar-item active'>About</NavLink>
+					</div>
+				</div>
+			</nav>
 			<div className="container">
-				<NavLink to="/" className="nav-link" exact={true} activeClassName='subtitle'>Home</NavLink>&nbsp;
-				<NavLink to="/about" className="nav-link" exact={true} activeClassName='subtitle'>About</NavLink>
-				
 				<Route path="/" exact component={Home} />
 				<Route path="/about" component={About} />
 			</div>
-		</Router>
+		</Router >
 	);
 }
 
