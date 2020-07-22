e69a5aab29474310489a32e7d23451b8b2f28afe
Cristian Leguizamón <cristianleguizamon37@gmail.com>
2020-07-22 13:16:46 -0300

    Botón para cerrar sesión fue trasladado al dropdown del usuario

--- web/src/sites/import/Navbar.js
+++ web/src/sites/import/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 import '../../style.css';
 import { connect } from 'react-redux'
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import {CERRAR_SESION} from "../../constantes/actionRedux"
-import {ADMIN, EMPLEADO} from '../../constantes/tiposUsuarios'
+import { faUser, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { CERRAR_SESION } from "../../constantes/actionRedux"
+import { ADMIN, EMPLEADO } from '../../constantes/tiposUsuarios'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Navbar extends React.Component {
@@ -14,8 +14,8 @@ class Navbar extends React.Component {
 
         };
     }
-    cerrarSesion = () =>{
-        const {cerrarSesion} = this.props
+    cerrarSesion = () => {
+        const { cerrarSesion } = this.props
         cerrarSesion()
     }
 
@@ -23,8 +23,8 @@ class Navbar extends React.Component {
         const { usuario, tipoUsuario } = this.props
 
         if (tipoUsuario != null) {
-            switch(tipoUsuario){
-                case ADMIN:{
+            switch (tipoUsuario) {
+                case ADMIN: {
                     return (
                         <nav className="navbar" role="navigation" aria-label="main navigation">
                             <div className="navbar-brand">
@@ -37,40 +37,50 @@ class Navbar extends React.Component {
                             <div className="navbar-start">
                                 <NavLink to="/" className="navbar-item" exact={true} activeClassName='navbar-item active'>Inicio</NavLink>
                             </div>
-        
+
                             <div id="navbarBasicExample" class="navbar-menu">
                                 <div class="navbar-item has-dropdown is-hoverable">
                                     <a class="navbar-link">
-                                    Administrador de Personal
+                                        Administrador de Personal
                                     </a>
 
                                     <div class="navbar-dropdown">
-                                    <a class="navbar-item" href="/listado/ES">
-                                        Control E/S
+                                        <a class="navbar-item" href="/listado/ES">
+                                            Control E/S
                                     </a>
-                                    <a class="navbar-item" href="/listado/recepcion">
-                                        Recepción
+                                        <a class="navbar-item" href="/listado/recepcion">
+                                            Recepción
                                     </a>
+                                    </div>
                                 </div>
-                            </div>                               
 
                                 <div className="navbar-end">
-                                    <div className="navbar-item has-dropdown is-hoverable">
-                                        <div className="navbar-item">
-                                            <span className="icon has-text-info">
-                                                <FontAwesomeIcon icon={faUser} />
-                                            </span>
-                                            <span>{usuario}</span>
-                                        </div>
-                                        <div className="navbar-dropdown">
-                                            <p class="navbar-item">
-                                                {tipoUsuario}
-                                            </p>
-                                        </div>
-                                    </div>
                                     <div className="navbar-item">
                                         <div className="buttons">
-                                            <NavLink to="/login" onClick={() => this.cerrarSesion()} className="button is-danger" activeClassName='button is-warning'>Cerrar sesión</NavLink>                             
+                                            <div className="dropdown is-hoverable is-right">
+                                                <div className="dropdown-trigger">
+                                                    <button className="button is-primary is-outlined is-rounded" aria-haspopup="true" aria-controls="dropdown-menu2">
+                                                        <span className="icon has-text-info">
+                                                            <FontAwesomeIcon icon={faUser} />
+                                                        </span>
+                                                        <span>{usuario}</span>
+                                                        <span class="icon is-small">
+                                                            <FontAwesomeIcon icon={faAngleDown} />
+                                                        </span>
+                                                    </button>
+                                                </div>
+                                                <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                                                    <div className="dropdown-content">
+                                                        <p className="dropdown-item">
+                                                            {tipoUsuario}
+                                                        </p>
+                                                        <hr className="dropdown-divider" />
+                                                        <p className="dropdown-item" style={{ margin: -15, marginBottom: -30 }}>
+                                                            <NavLink to="/login" onClick={() => this.cerrarSesion()} className="button is-danger" activeClassName='button is-danger' style={{ paddingLeft: 40, paddingRight: 40 }} >Cerrar sesión</NavLink>
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -78,7 +88,7 @@ class Navbar extends React.Component {
                         </nav>
                     )
                 }
-                case EMPLEADO:{
+                case EMPLEADO: {
                     return (
                         <nav className="navbar" role="navigation" aria-label="main navigation">
                             <div className="navbar-brand">
@@ -88,28 +98,38 @@ class Navbar extends React.Component {
                                     <span aria-hidden="true"></span>
                                 </a>
                             </div>
-        
+
                             <div id="navbarBasicExample" class="navbar-menu">
                                 <div className="navbar-start">
                                     <NavLink to="/" className="navbar-item" exact={true} activeClassName='navbar-item active'>Home</NavLink>
                                 </div>
                                 <div className="navbar-end">
-                                    <div className="navbar-item has-dropdown is-hoverable">
-                                        <div className="navbar-item">
-                                            <span className="icon has-text-info">
-                                                <FontAwesomeIcon icon={faUser} />
-                                            </span>
-                                            <span>{usuario}</span>
-                                        </div>
-                                        <div className="navbar-dropdown">
-                                            <p class="navbar-item">
-                                                {tipoUsuario}
-                                            </p>
-                                        </div>
-                                    </div>
                                     <div className="navbar-item">
                                         <div className="buttons">
-                                            <NavLink to="/login" onClick={() => this.cerrarSesion()} className="button is-danger" activeClassName='button is-warning'>Cerrar sesión</NavLink>                             
+                                            <div className="dropdown is-hoverable is-right">
+                                                <div className="dropdown-trigger">
+                                                    <button className="button is-primary is-outlined is-rounded" aria-haspopup="true" aria-controls="dropdown-menu2">
+                                                        <span className="icon has-text-info">
+                                                            <FontAwesomeIcon icon={faUser} />
+                                                        </span>
+                                                        <span>{usuario}</span>
+                                                        <span class="icon is-small">
+                                                            <FontAwesomeIcon icon={faAngleDown} />
+                                                        </span>
+                                                    </button>
+                                                </div>
+                                                <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                                                    <div className="dropdown-content">
+                                                        <p className="dropdown-item">
+                                                            {tipoUsuario}
+                                                        </p>
+                                                        <hr className="dropdown-divider" />
+                                                        <p className="dropdown-item" style={{ margin: -15, marginBottom: -30 }}>
+                                                            <NavLink to="/login" onClick={() => this.cerrarSesion()} className="button is-danger" activeClassName='button is-danger' style={{ paddingLeft: 40, paddingRight: 40 }} >Cerrar sesión</NavLink>
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
