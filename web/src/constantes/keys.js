be60136f3becd636496fa74a3519bc5cc21a6bfd
Cristian Leguizamón <cristianleguizamon37@gmail.com>
2020-07-22 11:09:33 -0300

    Integración con API de Google Maps

---
+++ web/src/constantes/keys.js
@@ -0,0 +1,1 @@
+export const GOOGLE_MAPS_API_KEY = 'AIzaSyDlPduN72Il8JfWrPio3VxBV7BZhDswRfg'
--- web/src/sites/Home.js
+++ web/src/sites/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { ADMIN, EMPLEADO } from '../constantes/tiposUsuarios'
+import { GOOGLE_MAPS_API_KEY } from '../constantes/keys'
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
 
 class Home extends React.Component {
 	constructor() {
@@ -18,10 +20,31 @@ class Home extends React.Component {
 				return (
 					<React.Fragment>
 						<div className="hero-body">
-							<p className="title">Sos un empleado</p>
-							<p className="subtitle">Datos de tu sucursal</p>
+							<p className="title">{'Hola ' + usuario}</p>
 						</div>
-					</React.Fragment>
+						<div className="container" style={{ flex: 1, backgroundColor: 'white', padding: '20px', borderRadius: '15px', marginRight: '15px', marginLeft: '15px', marginBottom: '15px' }}>
+							<div className="columns">
+								<div className="column" style={{}}>
+									<p className="title">{'Sucursal'}</p>
+									<p className="subtitle">{'Dirección'}</p>
+								</div>
+								<div className="column" style={{ }}>
+									<Map
+										google={this.props.google}
+										zoom={10}
+										initialCenter={{
+											lat: -34.6144934119689,
+											lng: -58.4458563545429
+										}}
+										containerStyle={{ position: 'relative' }}
+										style={{ height: '100%' }}
+										>
+											<Marker name={'Current location'} />
+									</Map>
+								</div>
+							</div>
+						</div>
+					</React.Fragment >
 				);
 			case ADMIN:
 				return (
@@ -52,4 +75,4 @@ const mapStateToProps = (state) => {
 
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default connect(mapStateToProps, mapDispatchToProps)(GoogleApiWrapper({ apiKey: (GOOGLE_MAPS_API_KEY) })(Home))
