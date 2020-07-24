2050fe3748077361e89a4791dbfba3e6779b520e
Cristian Leguizamón <cristianleguizamon37@gmail.com>
2020-07-24 11:14:41 -0300

    Uso de Semantic UI y Bulma simultáneamente y correcciones en estilos de tablas y h1

--- web/src/sites/listados/tablas/ControlES.js
+++ web/src/sites/listados/tablas/ControlES.js
@@ -12,10 +12,10 @@ export const HeaderES = () => {
     return (
         <thead>
             <tr>
-                <th style={{ padding: '12px' }}>Nombre</th>
-                <th style={{ padding: '12px' }}>Apellido</th>
-                <th style={{ padding: '12px' }}>Rol</th>
-                <th style={{ padding: '12px' }}>Mail</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Rol</th>
+                <th>Mail</th>
                 <th></th>
             </tr>
         </thead>
