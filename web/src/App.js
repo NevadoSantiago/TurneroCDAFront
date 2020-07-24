fad016c14b0251835fe4b128bbea101c5f7a51cf
Santiago <nevadosantiago97@gmail.com>
2020-07-24 11:29:35 -0300

    navBar dividida, empleado--> contro, recepcion

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AdministrarPersonal from './sites/AdministrarPersonal'
-import { ADMIN, EMPLEADO } from "./constantes/tiposUsuarios"
+import { ADMIN_SUCURSAL,ADMIN_GENERAL,CONTROL_ES,RECEPCION } from "./constantes/tiposUsuarios"
 import NoAutorizado from './sites/NoAutorizado'
 import Login from './sites/Login'
 import Home from './sites/Home'
@@ -22,12 +22,12 @@ class App extends React.Component {
 					<Switch>
 						<Route exact path="/login" component={Login}></Route>
 						<Route path="/noAutorizado" component={NoAutorizado}></Route>
-						<AuthRoutePrivate path="/nuevo" component={NuevoTurno} autorizado={ADMIN} autorizado2={EMPLEADO} ></AuthRoutePrivate>
-						<AuthRoutePrivate path="/editar" component={EditarPersona} autorizado={ADMIN} autorizado2={EMPLEADO} ></AuthRoutePrivate>
-						<AuthRoutePrivate path="/listaES" exact component={ControlES} autorizado={ADMIN} autorizado2={null} ></AuthRoutePrivate>
-						<AuthRoutePrivate path="/listaRecepcion" exact component={Recepcion} autorizado={ADMIN} autorizado2={null} ></AuthRoutePrivate>
-						<AuthRoutePrivate path="/administrarPersona" exact component={AdministrarPersonal} autorizado={ADMIN} autorizado2={null}></AuthRoutePrivate>
-						<AuthRoutePrivate path="/" component={Home} autorizado={ADMIN} autorizado2={EMPLEADO} ></AuthRoutePrivate>
+						<AuthRoutePrivate path="/nuevo" component={NuevoTurno} autorizado={ADMIN_SUCURSAL} autorizado2={CONTROL_ES} autorizado3={RECEPCION} ></AuthRoutePrivate>
+						<AuthRoutePrivate path="/editar" component={EditarPersona} autorizado={ADMIN_SUCURSAL} autorizado2={CONTROL_ES} autorizado3={RECEPCION}  ></AuthRoutePrivate>
+						<AuthRoutePrivate path="/listaES" exact component={ControlES} autorizado={ADMIN_SUCURSAL} autorizado2={null} autorizado3={null}></AuthRoutePrivate>
+						<AuthRoutePrivate path="/listaRecepcion" exact component={Recepcion} autorizado={ADMIN_SUCURSAL} autorizado2={null} autorizado3={null}  ></AuthRoutePrivate>
+						<AuthRoutePrivate path="/administrarPersona" exact component={AdministrarPersonal} autorizado={ADMIN_SUCURSAL} autorizado2={null} autorizado3={null} ></AuthRoutePrivate>
+						<AuthRoutePrivate path="/" component={Home} autorizado={ADMIN_SUCURSAL} autorizado2={CONTROL_ES} autorizado3={RECEPCION}  ></AuthRoutePrivate>
 					</Switch>
 				</div>
 			</Router>
--- web/src/sites/EditarPersona.js
+++ web/src/sites/EditarPersona.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux'
 import Error from '../servicios/alertas/Error'
 import Correcto from '../servicios/alertas/Correcto'
-import { URL_API_RESERVA } from '../constantes/urlApi'
-import {getRolesDeUsuario} from '../servicios/EmpleadoServices'
-import {editarDatos} from '../servicios/AdminServices'
+import { getRolesDeUsuario } from '../servicios/EmpleadoServices'
+import { editarDatos } from '../servicios/AdminServices'
 
 class EditarPersona extends React.Component {
     constructor() {
@@ -12,7 +11,7 @@ class EditarPersona extends React.Component {
         this.state = {
             error: null,
             correcto: null,
-            roles:null
+            roles: null
         };
     }
     mostrarOpcion = (opcion) => {
@@ -20,36 +19,36 @@ class EditarPersona extends React.Component {
             <option value={opcion.tipoId}>{opcion.detalle}</option>
         )
     }
-    getRoles = async () =>{
+    getRoles = async () => {
         const roles = await getRolesDeUsuario()
         this.setState({
             roles
         })
     }
-    componentDidMount () {
+    componentDidMount() {
         this.getRoles()
     }
-    editarDatos =async e =>{
-        const {location} = this.props
-        const seEdito =await editarDatos(e,location)
-        if(seEdito){
+    editarDatos = async e => {
+        const { location } = this.props
+        const seEdito = await editarDatos(e, location)
+        if (seEdito) {
             this.setState({
-                correcto:"Se edito el usuario correctamente"
+                correcto: "Se edito el usuario correctamente"
             })
-            setTimeout(()=>{this.props.history.goBack()}, 1500)
-        }else{
+            setTimeout(() => { this.props.history.goBack() }, 1500)
+        } else {
             this.setState({
-                error:"Hubo un error al editar"
+                error: "Hubo un error al editar"
             })
         }
     }
 
     render() {
-        const { error, correcto,roles } = this.state
+        const { error, correcto, roles } = this.state
         const { location } = this.props
 
-        if (location.user != null && roles !=null) {
-            const {user} = location
+        if (location.user != null && roles != null) {
+            const { user } = location
             return (
                 <React.Fragment>
                     <div className="hero-body">
@@ -83,23 +82,23 @@ class EditarPersona extends React.Component {
                                             <div className="select is-fullwidth is-black">
                                                 <select name="rol">
                                                     {
-                                                        roles.map((r, i) =>{
-                                                            if(location.user.rol == r.detalle){
-                                                                return(
-                                                                <option 
-                                                                selected 
-                                                                value={r.tipoId}>
-                                                                    {r.detalle}
-                                                                </option>  
+                                                        roles.map((r, i) => {
+                                                            if (location.user.rol == r.detalle) {
+                                                                return (
+                                                                    <option
+                                                                        selected
+                                                                        value={r.tipoId}>
+                                                                        {r.detalle}
+                                                                    </option>
                                                                 )
-                                                            }else{
-                                                                return(
-                                                                     this.mostrarOpcion(r)
+                                                            } else {
+                                                                return (
+                                                                    this.mostrarOpcion(r)
                                                                 )
                                                             }
-                                                           
+
                                                         }
-                                                            
+
                                                         )
                                                     }
                                                 </select>
