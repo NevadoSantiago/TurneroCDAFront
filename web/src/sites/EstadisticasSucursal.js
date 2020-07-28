58c2a0aae0ed156a632b348db730acd04946f690
Cristian Leguizamón <cristianleguizamon37@gmail.com>
2020-07-28 11:39:37 -0300

    Agregar placeholder para simular carga de pantalla

--- web/src/sites/EstadisticasSucursal.js
+++ web/src/sites/EstadisticasSucursal.js
@@ -11,6 +11,7 @@ class EstadisticasSucursal extends React.Component {
     this.state = {
       dataEspecialidades: null,
       listaDeEspera: null,
+      loadingStats: true,
     };
   }
 
@@ -21,13 +22,15 @@ class EstadisticasSucursal extends React.Component {
     this.setState({
       listaDeEspera,
     });
+
+    return listaDeEspera;
   };
 
   checkIfReservaHasEspecialidad = async (especialidad) => {
     const { location } = this.props;
     const { sucursal } = location;
-    const listaDeEspera = await getListaDeEspera(sucursal.sucursalId);
 
+    const listaDeEspera = await getListaDeEspera(sucursal.sucursalId);
     var count = 0;
 
     await listaDeEspera.map((persona) => {
@@ -83,6 +86,11 @@ class EstadisticasSucursal extends React.Component {
   componentDidMount() {
     this.getListaDeEspera();
     this.interval = setInterval(() => this.getListaDeEspera(), 10000);
+    setInterval(() => {
+      this.setState({
+        loadingStats: false,
+      });
+    }, 1000);
   }
 
   componentWillUnmount() {
@@ -91,7 +99,7 @@ class EstadisticasSucursal extends React.Component {
 
   render() {
     const { location, especialidades } = this.props;
-    const { dataEspecialidades, listaDeEspera } = this.state;
+    const { dataEspecialidades, listaDeEspera, loadingStats } = this.state;
 
     defaults.global.defaultFontFamily = "Nunito";
     defaults.global.defaultFontStyle = "bold";
@@ -105,132 +113,218 @@ class EstadisticasSucursal extends React.Component {
       listaDeEspera != null
     ) {
       const { sucursal } = location;
-      return (
-        <React.Fragment>
-          <div className="hero-body">
-            <p className="title">{"Estadísticas"}</p>
-            <p className="subtitle">
-              <b>{"(" + sucursal.sucursalId + ")"}</b>
-            </p>
-          </div>
-          <div
-            className="container"
-            style={{
-              flex: 1,
-              backgroundColor: "white",
-              padding: "20px",
-              borderRadius: "15px",
-              marginRight: "15px",
-              marginLeft: "15px",
-              marginBottom: "15px",
-            }}
-          >
-            <button
-              className="button is-rounded is-danger is-outlined"
-              style={{ float: "right", marginTop: "-5px", marginRight: "-5px" }}
+      if (loadingStats) {
+        return (
+          <React.Fragment>
+            <div className="hero-body">
+              <p className="title">{"Estadísticas"}</p>
+              <p className="subtitle">
+                <b>{"(" + sucursal.sucursalId + ")"}</b>
+              </p>
+            </div>
+            <div
+              className="container"
+              style={{
+                flex: 1,
+                backgroundColor: "white",
+                padding: "20px",
+                borderRadius: "15px",
+                marginRight: "15px",
+                marginLeft: "15px",
+                marginBottom: "15px",
+              }}
             >
-              <span class="icon is-small">
-                <FontAwesomeIcon icon={faFilePdf} />
-              </span>
-              <span>Generar reporte PDF</span>
-            </button>
-            <p className="subtitle">
-              {"Sucursal: "}
-              <b>{sucursal.nombre}</b>
-              <br />
-              {"Dirección: "}
-              {sucursal.direccion}
-            </p>
-            <div className="columns">
-              <div className="column is-6" style={{ textAlign: "center" }}>
-                <p className="subtitle">
-                  <b>{"Turnos según especialidad"}</b>
-                </p>
-                <Pie
-                  data={dataEspecialidades}
-                  width={10}
-                  height={5}
-                  options={{
-                    responsive: true,
-                    maintainAspectRatio: true,
-                    legend: {
-                      position: "left",
-                    },
-                  }}
-                />
+              <button
+                className="button is-rounded is-danger is-outlined"
+                style={{
+                  float: "right",
+                  marginTop: "-5px",
+                  marginRight: "-5px",
+                }}
+              >
+                <span class="icon is-small">
+                  <FontAwesomeIcon icon={faFilePdf} />
+                </span>
+                <span>Generar reporte PDF</span>
+              </button>
+              <p className="subtitle">
+                {"Sucursal: "}
+                <b>{sucursal.nombre}</b>
+                <br />
+                {"Dirección: "}
+                {sucursal.direccion}
+              </p>
+              <div className="columns">
+                <div className="column is-6">
+                  <div className="ui placeholder">
+                    <div className="header">
+                      <div className="medium line"></div>
+                    </div>
+                    <div className="image" style={{ height: "250px" }}></div>
+                  </div>
+                </div>
+                <div className="column is-6">
+                  <div className="ui placeholder">
+                    <div className="header">
+                      <div className="medium line"></div>
+                    </div>
+                    <div className="image" style={{ height: "250px" }}></div>
+                  </div>
+                </div>
               </div>
-              <div className="column is-6" style={{ textAlign: "center" }}>
-                <p className="subtitle">
-                  <b>{"Gráfico 2"}</b>
-                </p>
-                <Pie
-                  data={{
-                    datasets: [
-                      {
-                        data: [10, 20, 30],
-                        backgroundColor: ["lightblue", "lavender", "pink"],
-                      },
-                    ],
-                    // These labels appear in the legend and in the tooltips when hovering different arcs
-                    labels: ["Light blue", "Lavender", "Pink"],
-                  }}
-                  width={10}
-                  height={5}
-                  options={{ responsive: true, maintainAspectRatio: true }}
-                />
-              </div>
-            </div>
-            <div className="columns is-centered">
-              <div className="column is-9" style={{ textAlign: "center" }}>
-                <p className="subtitle">
-                  <b>{"Gráfico 3"}</b>
-                </p>
-                <Bar
-                  data={{
-                    labels: [
-                      "Red",
-                      "Blue",
-                      "Yellow",
-                      "Green",
-                      "Purple",
-                      "Orange",
-                    ],
-                    datasets: [
-                      {
-                        label: "# of Votes",
-                        data: [12, 19, 3, 5, 2, 3],
-                        backgroundColor: [
-                          "rgba(255, 99, 132, 0.2)",
-                          "rgba(54, 162, 235, 0.2)",
-                          "rgba(255, 206, 86, 0.2)",
-                          "rgba(75, 192, 192, 0.2)",
-                          "rgba(153, 102, 255, 0.2)",
-                          "rgba(255, 159, 64, 0.2)",
-                        ],
-                        borderColor: [
-                          "rgba(255, 99, 132, 1)",
-                          "rgba(54, 162, 235, 1)",
-                          "rgba(255, 206, 86, 1)",
-                          "rgba(75, 192, 192, 1)",
-                          "rgba(153, 102, 255, 1)",
-                          "rgba(255, 159, 64, 1)",
-                        ],
-                        borderWidth: 1,
-                      },
-                    ],
-                  }}
-                  width={10}
-                  height={5}
-                  options={{
-                    responsive: true,
-                    maintainAspectRatio: true,
-                  }}
-                />
+              <div className="columns">
+                <div className="column is-6">
+                  <div className="ui placeholder">
+                    <div className="header">
+                      <div className="medium line"></div>
+                    </div>
+                    <div className="image" style={{ height: "250px" }}></div>
+                  </div>
+                </div>
+                <div className="column is-6">
+                  <div className="ui placeholder">
+                    <div className="header">
+                      <div className="medium line"></div>
+                    </div>
+                    <div className="image" style={{ height: "250px" }}></div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </React.Fragment>
-      );
+          </React.Fragment>
+        );
+      } else {
+        return (
+          <React.Fragment>
+            <div className="hero-body">
+              <p className="title">{"Estadísticas"}</p>
+              <p className="subtitle">
+                <b>{"(" + sucursal.sucursalId + ")"}</b>
+              </p>
+            </div>
+            <div
+              className="container"
+              style={{
+                flex: 1,
+                backgroundColor: "white",
+                padding: "20px",
+                borderRadius: "15px",
+                marginRight: "15px",
+                marginLeft: "15px",
+                marginBottom: "15px",
+              }}
+            >
+              <button
+                className="button is-rounded is-danger is-outlined"
+                style={{
+                  float: "right",
+                  marginTop: "-5px",
+                  marginRight: "-5px",
+                }}
+              >
+                <span class="icon is-small">
+                  <FontAwesomeIcon icon={faFilePdf} />
+                </span>
+                <span>Generar reporte PDF</span>
+              </button>
+              <p className="subtitle">
+                {"Sucursal: "}
+                <b>{sucursal.nombre}</b>
+                <br />
+                {"Dirección: "}
+                {sucursal.direccion}
+              </p>
+              <div className="columns">
+                <div className="column is-6" style={{ textAlign: "center" }}>
+                  <p className="subtitle">
+                    <b>{"Turnos según especialidad"}</b>
+                  </p>
+                  <Pie
+                    data={dataEspecialidades}
+                    width={10}
+                    height={5}
+                    options={{
+                      responsive: true,
+                      maintainAspectRatio: true,
+                      legend: {
+                        position: "left",
+                      },
+                    }}
+                  />
+                </div>
+                <div className="column is-6" style={{ textAlign: "center" }}>
+                  <p className="subtitle">
+                    <b>{"Gráfico 2"}</b>
+                  </p>
+                  <Pie
+                    data={{
+                      datasets: [
+                        {
+                          data: [10, 20, 30],
+                          backgroundColor: ["lightblue", "lavender", "pink"],
+                        },
+                      ],
+                      // These labels appear in the legend and in the tooltips when hovering different arcs
+                      labels: ["Light blue", "Lavender", "Pink"],
+                    }}
+                    width={10}
+                    height={5}
+                    options={{ responsive: true, maintainAspectRatio: true }}
+                  />
+                </div>
+              </div>
+              <div className="columns is-centered">
+                <div className="column is-9" style={{ textAlign: "center" }}>
+                  <p className="subtitle">
+                    <b>{"Gráfico 3"}</b>
+                  </p>
+                  <Bar
+                    data={{
+                      labels: [
+                        "Red",
+                        "Blue",
+                        "Yellow",
+                        "Green",
+                        "Purple",
+                        "Orange",
+                      ],
+                      datasets: [
+                        {
+                          label: "# of Votes",
+                          data: [12, 19, 3, 5, 2, 3],
+                          backgroundColor: [
+                            "rgba(255, 99, 132, 0.2)",
+                            "rgba(54, 162, 235, 0.2)",
+                            "rgba(255, 206, 86, 0.2)",
+                            "rgba(75, 192, 192, 0.2)",
+                            "rgba(153, 102, 255, 0.2)",
+                            "rgba(255, 159, 64, 0.2)",
+                          ],
+                          borderColor: [
+                            "rgba(255, 99, 132, 1)",
+                            "rgba(54, 162, 235, 1)",
+                            "rgba(255, 206, 86, 1)",
+                            "rgba(75, 192, 192, 1)",
+                            "rgba(153, 102, 255, 1)",
+                            "rgba(255, 159, 64, 1)",
+                          ],
+                          borderWidth: 1,
+                        },
+                      ],
+                    }}
+                    width={10}
+                    height={5}
+                    options={{
+                      responsive: true,
+                      maintainAspectRatio: true,
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          </React.Fragment>
+        );
+      }
     } else {
       return (
         <div className="hero-body">
