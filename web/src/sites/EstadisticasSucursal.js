12bbd5ee2b35203c3b7ba710b0b7965c52c57e40
Cristian Leguizamón <cristianleguizamon37@gmail.com>
2020-07-28 14:44:48 -0300

    test

--- web/src/sites/EstadisticasSucursal.js
+++ web/src/sites/EstadisticasSucursal.js
@@ -4,6 +4,7 @@ import { Pie, Bar, defaults } from "react-chartjs-2";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
 import { getListaDeEspera } from "../servicios/EmpleadoServices";
+import { NavLink } from "react-router-dom";
 
 class EstadisticasSucursal extends React.Component {
   constructor() {
@@ -95,6 +96,52 @@ class EstadisticasSucursal extends React.Component {
 
   componentWillUnmount() {
     clearInterval(this.interval);
+  }
+
+  generatePDF(e) {
+    e.preventDefault();
+
+    function addElementHTML(parentId, elementTag, elementId, html) {
+      // Adds an element to the document
+      var p = document.getElementById(parentId);
+      var newElement = document.createElement(elementTag);
+      newElement.setAttribute("id", elementId);
+      newElement.async = true;
+      newElement.innerHTML = html;
+      p.appendChild(newElement);
+    }
+
+    /*var js = `
+    html2canvas(document.querySelector("#capture")), {
+      scale: 2,
+      onrendered: function(canvas){
+        var imgData = canvas.toDataURL('image/png');
+
+        var pdf = new jsPDF();
+        pdf.addImage(imgData, 'PNG', 0, 0);
+        pdf.save("download.pdf");
+        console.log('Llegué')
+        //pdf.output('dataurlnewwindow');
+      }
+    }
+    `;*/
+    var js = `
+    console.log('Afuera')
+    html2canvas(document.getElementById("capture"), {
+      dpi: 1600, // Set to 1600 DPI
+      scale: 0.78, // Adjusts your resolution
+    }).then(function(canvas) {
+      console.log('Adentro')
+      var w = document.getElementById("root").offsetWidth;
+      var h = document.getElementById("root").offsetHeight;
+      var img = canvas.toDataURL("image/jpeg");
+      var doc = new jsPDF('p', 'mm', 'a4');
+      doc.addImage({imageData: img, format: 'JPEG', compression: 'NONE'})
+      doc.save('descarga.pdf');
+    });
+    `;
+
+    addElementHTML("scripts", "script", "html2canvasJS", js);
   }
 
   render() {
@@ -196,129 +243,133 @@ class EstadisticasSucursal extends React.Component {
       } else {
         return (
           <React.Fragment>
-            <div className="hero-body">
-              <p className="title">{"Estadísticas"}</p>
-              <p className="subtitle">
-                <b>{"(" + sucursal.sucursalId + ")"}</b>
-              </p>
-            </div>
-            <div
-              className="container"
-              style={{
-                flex: 1,
-                backgroundColor: "white",
-                padding: "20px",
-                borderRadius: "15px",
-                marginRight: "15px",
-                marginLeft: "15px",
-                marginBottom: "15px",
-              }}
-            >
-              <button
-                className="button is-rounded is-danger is-outlined"
+            <div id="capture">
+              <div className="hero-body">
+                <p className="title">{"Estadísticas"}</p>
+                <p className="subtitle">
+                  <b>{"(" + sucursal.sucursalId + ")"}</b>
+                </p>
+              </div>
+              <div
+                className="container"
                 style={{
-                  float: "right",
-                  marginTop: "-5px",
-                  marginRight: "-5px",
+                  flex: 1,
+                  backgroundColor: "white",
+                  padding: "20px",
+                  borderRadius: "15px",
+                  marginRight: "15px",
+                  marginLeft: "15px",
+                  marginBottom: "15px",
                 }}
               >
-                <span class="icon is-small">
-                  <FontAwesomeIcon icon={faFilePdf} />
-                </span>
-                <span>Generar reporte PDF</span>
-              </button>
-              <p className="subtitle">
-                {"Sucursal: "}
-                <b>{sucursal.nombre}</b>
-                <br />
-                {"Dirección: "}
-                {sucursal.direccion}
-              </p>
-              <div className="columns">
-                <div className="column is-6" style={{ textAlign: "center" }}>
-                  <p className="subtitle">
-                    <b>{"Turnos según especialidad"}</b>
-                  </p>
-                  <Pie
-                    data={dataEspecialidades}
-                    width={10}
-                    height={5}
-                    options={{
-                      responsive: true,
-                      maintainAspectRatio: true,
-                      legend: {
-                        position: "left",
-                      },
-                    }}
-                  />
-                </div>
-                <div className="column is-6" style={{ textAlign: "center" }}>
-                  <p className="subtitle">
-                    <b>{"Gráfico 2"}</b>
-                  </p>
-                  <Pie
-                    data={{
-                      datasets: [
-                        {
-                          data: [10, 20, 30],
-                          backgroundColor: ["lightblue", "lavender", "pink"],
+                <button
+                  className="button is-rounded is-danger is-outlined"
+                  data-html2canvas-ignore="true"
+                  onClick={(e) => this.generatePDF(e)}
+                  style={{
+                    float: "right",
+                    marginTop: "-5px",
+                    marginRight: "-5px",
+                  }}
+                >
+                  <span class="icon is-small">
+                    <FontAwesomeIcon icon={faFilePdf} />
+                  </span>
+                  <span>Generar reporte PDF</span>
+                </button>
+                <p className="subtitle">
+                  {"Sucursal: "}
+                  <b>{sucursal.nombre}</b>
+                  <br />
+                  {"Dirección: "}
+                  {sucursal.direccion}
+                </p>
+                <div className="columns">
+                  <div className="column is-6" style={{ textAlign: "center" }}>
+                    <p className="subtitle">
+                      <b>{"Turnos según especialidad"}</b>
+                    </p>
+                    <Pie
+                      data={dataEspecialidades}
+                      width={10}
+                      height={5}
+                      options={{
+                        responsive: true,
+                        maintainAspectRatio: true,
+                        legend: {
+                          position: "left",
                         },
-                      ],
-                      // These labels appear in the legend and in the tooltips when hovering different arcs
-                      labels: ["Light blue", "Lavender", "Pink"],
-                    }}
-                    width={10}
-                    height={5}
-                    options={{ responsive: true, maintainAspectRatio: true }}
-                  />
+                      }}
+                    />
+                  </div>
+                  <div className="column is-6" style={{ textAlign: "center" }}>
+                    <p className="subtitle">
+                      <b>{"Gráfico 2"}</b>
+                    </p>
+                    <Pie
+                      data={{
+                        datasets: [
+                          {
+                            data: [10, 20, 30],
+                            backgroundColor: ["lightblue", "lavender", "pink"],
+                          },
+                        ],
+                        // These labels appear in the legend and in the tooltips when hovering different arcs
+                        labels: ["Light blue", "Lavender", "Pink"],
+                      }}
+                      width={10}
+                      height={5}
+                      options={{ responsive: true, maintainAspectRatio: true }}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="columns is-centered">
-                <div className="column is-9" style={{ textAlign: "center" }}>
-                  <p className="subtitle">
-                    <b>{"Gráfico 3"}</b>
-                  </p>
-                  <Bar
-                    data={{
-                      labels: [
-                        "Red",
-                        "Blue",
-                        "Yellow",
-                        "Green",
-                        "Purple",
-                        "Orange",
-                      ],
-                      datasets: [
-                        {
-                          label: "# of Votes",
-                          data: [12, 19, 3, 5, 2, 3],
-                          backgroundColor: [
-                            "rgba(255, 99, 132, 0.2)",
-                            "rgba(54, 162, 235, 0.2)",
-                            "rgba(255, 206, 86, 0.2)",
-                            "rgba(75, 192, 192, 0.2)",
-                            "rgba(153, 102, 255, 0.2)",
-                            "rgba(255, 159, 64, 0.2)",
-                          ],
-                          borderColor: [
-                            "rgba(255, 99, 132, 1)",
-                            "rgba(54, 162, 235, 1)",
-                            "rgba(255, 206, 86, 1)",
-                            "rgba(75, 192, 192, 1)",
-                            "rgba(153, 102, 255, 1)",
-                            "rgba(255, 159, 64, 1)",
-                          ],
-                          borderWidth: 1,
-                        },
-                      ],
-                    }}
-                    width={10}
-                    height={5}
-                    options={{
-                      responsive: true,
-                      maintainAspectRatio: true,
-                    }}
-                  />
+                <div className="columns is-centered">
+                  <div className="column is-9" style={{ textAlign: "center" }}>
+                    <p className="subtitle">
+                      <b>{"Gráfico 3"}</b>
+                    </p>
+                    <Bar
+                      data={{
+                        labels: [
+                          "Red",
+                          "Blue",
+                          "Yellow",
+                          "Green",
+                          "Purple",
+                          "Orange",
+                        ],
+                        datasets: [
+                          {
+                            label: "# of Votes",
+                            data: [12, 19, 3, 5, 2, 3],
+                            backgroundColor: [
+                              "rgba(255, 99, 132, 0.2)",
+                              "rgba(54, 162, 235, 0.2)",
+                              "rgba(255, 206, 86, 0.2)",
+                              "rgba(75, 192, 192, 0.2)",
+                              "rgba(153, 102, 255, 0.2)",
+                              "rgba(255, 159, 64, 0.2)",
+                            ],
+                            borderColor: [
+                              "rgba(255, 99, 132, 1)",
+                              "rgba(54, 162, 235, 1)",
+                              "rgba(255, 206, 86, 1)",
+                              "rgba(75, 192, 192, 1)",
+                              "rgba(153, 102, 255, 1)",
+                              "rgba(255, 159, 64, 1)",
+                            ],
+                            borderWidth: 1,
+                          },
+                        ],
+                      }}
+                      width={10}
+                      height={5}
+                      options={{
+                        responsive: true,
+                        maintainAspectRatio: true,
+                      }}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
