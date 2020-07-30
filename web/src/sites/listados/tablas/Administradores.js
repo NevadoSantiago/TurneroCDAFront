b934a5d049c7e46b8010d1f2e0149c135814a831
Cristian Leguizamón <cristianleguizamon37@gmail.com>
2020-07-30 13:33:54 -0300

    Estilo tabla Administradores

--- web/src/sites/listados/tablas/Administradores.js
+++ web/src/sites/listados/tablas/Administradores.js
@@ -1,71 +1,79 @@
-import React from 'react';
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import { connect } from 'react-redux'
-import { ELIMINAR_ADMINISTRADOR } from '../../../constantes/actionRedux'
-import {eliminarAdminDeSucursal} from '../../../servicios/AdminServices'
+import { connect } from "react-redux";
+import { ELIMINAR_ADMINISTRADOR } from "../../../constantes/actionRedux";
+import { eliminarAdminDeSucursal } from "../../../servicios/AdminServices";
 
 export const HeaderAdministradores = () => {
-    return (
-        <thead>
-            <tr>
-                <th>Nombre</th>
-                <th>Apellido</th>
-                <th>Mail</th>
-                <th></th>
-            </tr>
-        </thead>
-    )
-
-}
+  return (
+    <thead>
+      <tr>
+        <th>Nombre</th>
+        <th>Apellido</th>
+        <th>Mail</th>
+        <th></th>
+      </tr>
+    </thead>
+  );
+};
 class DatosAdministradores extends React.Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
+  }
 
+  eliminarAdministrador = async (admin) => {
+    const { eliminarAdmin } = this.props;
+    if (
+      window.confirm(
+        "Seguro que desea eliminar el administrador " +
+          admin.nombre +
+          " " +
+          admin.apellido +
+          "?"
+      )
+    ) {
+      await eliminarAdminDeSucursal(admin.idEmpleado);
+      {
+        this.props.refresh();
+      }
     }
-    
+  };
 
-    eliminarAdministrador = async (admin) => {      
-        const {eliminarAdmin} = this.props   
-        if (window.confirm("Seguro que desea eliminar el administrador " + admin.nombre + " " + admin.apellido + "?")) { 
-            await eliminarAdminDeSucursal(admin.idEmpleado)
-            {this.props.refresh()}
-        }
-    }
-
-    render() {
-        const { admin } = this.props
-        return (
-            <tbody>
-                <tr>
-                    <td>{admin.nombre}</td>
-                    <td>{admin.apellido}</td>
-                    <td>{admin.mail}</td>
-                    <td className="right aligned" style={{ padding: 0 }}>
-                        <button
-                            className="button is-danger is-outlined"
-                            onClick={() => this.eliminarAdministrador(admin)}
-                        >
-                            <span className="icon">
-                                <FontAwesomeIcon icon={faTrash} />
-                            </span>
-                            <span>Eliminar</span>
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        )
-    }
+  render() {
+    const { admin } = this.props;
+    return (
+      <tbody>
+        <tr>
+          <td style={{ padding: "1rem" }}>{admin.nombre}</td>
+          <td style={{ padding: "1rem" }}>{admin.apellido}</td>
+          <td style={{ padding: "1rem" }}>{admin.mail}</td>
+          <td className="right aligned" style={{ padding: "7px 10px 0px 0px" }}>
+            <button
+              className="button is-danger is-outlined"
+              onClick={() => this.eliminarAdministrador(admin)}
+            >
+              <span className="icon">
+                <FontAwesomeIcon icon={faTrash} />
+              </span>
+              <span>Eliminar</span>
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    );
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-    };
+  return {};
 };
 
 const mapStateToProps = (state) => {
-    return {
-    };
+  return {};
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(DatosAdministradores)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DatosAdministradores);
